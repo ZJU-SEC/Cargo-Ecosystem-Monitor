a152b677e7851b58a8fbe838567edebab12eeda6
LoanCold <loancold@foxmail.com>
2023-02-28 16:24:29 +0800

    Better organize RUF analysis scripts

--- Code/scripts/nightly_feature.sql
+++ Code/scripts/nightly_feature.sql
@@ -1,8 +1,16 @@
--- Nightly Feature Overview
+-- RUF Definition
+-- 1. All RUF
+SELECT COUNT(*) FROM feature_timeline;
+-- 2. RUF at last
+SELECT status, COUNT(*) FROM feature_status GROUP BY status;
+
+
+
+-- RUF Overview
 -- 1.Sample
 SELECT COUNT(*) FROM version_feature WHERE feature != 'no_feature_used';
 SELECT COUNT(DISTINCT id) FROM version_feature;
--- How many versions have nightly features
+-- How many versions have RUF
 SELECT COUNT(DISTINCT id) FROM version_feature WHERE feature != 'no_feature_used';
 SELECT COUNT(DISTINCT crate_id) FROM version_feature INNER JOIN versions 
     ON version_feature.id = versions.id  WHERE feature != 'no_feature_used';
@@ -12,7 +20,7 @@ SELECT COUNT(DISTINCT crate_id) FROM version_feature INNER JOIN versions
 SELECT COUNT(DISTINCT id) FROM version_feature WHERE conds LIKE 'feature = %' AND feature != 'no_feature_used';
 SELECT COUNT(DISTINCT crate_id) FROM version_feature INNER JOIN versions 
     ON version_feature.id = versions.id  WHERE conds LIKE 'feature = %' AND feature != 'no_feature_used';
--- Other unresolved features
+-- Other unresolved RUF
 SELECT * FROM version_feature WHERE conds like 'all(%'  LIMIT 100      ;
 
 -- 2. Type
@@ -27,16 +35,10 @@ SELECT COUNT(DISTINCT crate_id), status FROM ruf_ver INNER JOIN versions
     ON ruf_ver.id=versions.id GROUP BY status;
 
 
--- 3. Top
--- Hot features by usage
-WITH hot_feat AS(
-    SELECT feature, COUNT(*) FROM version_feature WHERE feature != 'no_feature_used' GROUP BY feature ORDER BY count DESC
-) SELECT feature, count, status FROM hot_feat INNER JOIN feature_status ON feature = name;
 
 
-
--- Nightly Feature Propagation
--- 1. Unconditional nightly features
+-- RUF Impacts/Propagation/IndirectDependents
+-- 1. Unconditional RUF
 -- Indir dependents
 SELECT COUNT(DISTINCT version_from) FROM dep_version WHERE version_to IN(
     SELECT id FROM version_feature WHERE conds = '' AND feature != 'no_feature_used'
@@ -57,7 +59,7 @@ WITH uncon_ver AS (
 ))
 SELECT COUNT(DISTINCT crate_id) FROM uncon_ver INNER JOIN versions
     ON version_to=id;
--- Hot version(RUF) with most dependents
+-- Hot version using RUF with most dependents
 WITH uncon_ver_sta AS (
 	WITH uncon_ver AS (
 		SELECT versions_with_name.id, name, num, feature FROM version_feature_ori INNER JOIN versions_with_name
@@ -73,7 +75,7 @@ FROM uncon_ver_sta INNER JOIN indir_dep ON id=version_to
 ORDER BY dependents DESC
 
 
--- 2. Conditional nightly features
+-- 2. Conditional RUF
 -- Possible indir dependents
 SELECT COUNT(DISTINCT version_from) FROM dep_version WHERE version_to IN(
     SELECT id FROM version_feature WHERE conds LIKE 'feature = %' AND feature != 'no_feature_used'
@@ -88,7 +90,7 @@ SELECT COUNT(DISTINCT version_from) FROM dep_version_feature;
 SELECT COUNT(DISTINCT crate_id) 
 FROM dep_version_feature INNER JOIN versions ON version_from = id;
 
--- 3. Total nightly features
+-- 3. Total RUF
 SELECT COUNT(DISTINCT version_from) 
 FROM dep_version_feature WHERE version_from NOT IN (
     SELECT DISTINCT version_from FROM dep_version WHERE version_to IN(
@@ -106,7 +108,7 @@ WHERE crate_id NOT IN (
 );
 
 
--- 3. Different types of nightly features
+-- 3. Different types of RUF
 SELECT status, COUNT((status)) FROM feature_status GROUP BY status ;
 SELECT  status, COUNT (DISTINCT(version_from)) FROM dep_version_feature 
 INNER JOIN feature_status ON name=nightly_feature GROUP BY status ;
@@ -114,58 +116,5 @@ SELECT COUNT(DISTINCT version_from) FROM dep_version WHERE version_to IN(
     SELECT id FROM version_feature INNER JOIN feature_status 
     ON name=feature WHERE conds = '' AND feature != 'no_feature_used' AND status = 'xxx'
 );
--- Top dependents
-WITH deps AS (
-    SELECT version_to, COUNT(DISTINCT version_from) as dependents
-    FROM dep_version
-    WHERE version_to IN(
-        SELECT id FROM version_feature INNER JOIN feature_status 
-        ON name=feature WHERE conds = '' AND feature != 'no_feature_used' AND status = 'xxx'
-    ) GROUP BY version_to
-)
-SELECT deps.*, crate_id, num ,name
-FROM deps INNER JOIN versions_with_name ON id=version_to ORDER BY dependents DESC;
 
 
-
-
--- RUF Evolution
-
-SELECT versions.id, feature, created_at FROM version_feature INNER JOIN versions
-ON version_feature.id = versions.id WHERE feature != 'no_feature_used' AND created_at < '2022-06-01' ORDER BY created_at DESC;
-
-DROP TABLE IF EXISTS tmp_year_month;
-CREATE TABLE tmp_year_month(
-    crate_date date
-);
-INSERT INTO tmp_year_month VALUES
-    ('2015-01-01'), ('2015-04-01'), ('2015-07-01'), ('2015-10-01'),
-    ('2016-01-01'), ('2016-04-01'), ('2016-07-01'), ('2016-10-01'),
-    ('2017-01-01'), ('2017-04-01'), ('2017-07-01'), ('2017-10-01'),
-    ('2018-01-01'), ('2018-04-01'), ('2018-07-01'), ('2018-10-01'),
-    ('2019-01-01'), ('2019-04-01'), ('2019-07-01'), ('2019-10-01'),
-    ('2020-01-01'), ('2020-04-01'), ('2020-07-01'), ('2020-10-01'),
-    ('2021-01-01'), ('2021-04-01'), ('2021-07-01'), ('2021-10-01'),
-    ('2022-01-01'), ('2022-04-01'), ('2022-07-01'), ('2022-08-12')
-;
-
--- Version (ruf)
-WITH all_ruf AS(
-  SELECT versions.id, feature, created_at FROM version_feature INNER JOIN versions
-ON version_feature.id = versions.id WHERE feature != 'no_feature_used'
-) 
-SELECT crate_date, COUNT(DISTINCT id) FROM all_ruf INNER JOIN tmp_year_month
-ON crate_date > created_at GROUP BY crate_date ORDER BY crate_date ASC;
-
--- Crate (ruf)
-WITH all_ruf AS(
-  SELECT versions_with_name.crate_id, feature, created_at FROM version_feature INNER JOIN versions_with_name
-ON version_feature.id = versions_with_name.id WHERE feature != 'no_feature_used'
-) 
-SELECT crate_date, COUNT(DISTINCT crate_id) FROM all_ruf INNER JOIN tmp_year_month
-ON crate_date > created_at GROUP BY crate_date ORDER BY crate_date ASC;
-
--- Version (all)
-
-SELECT crate_date, COUNT(DISTINCT id) FROM versions INNER JOIN tmp_year_month
-ON crate_date > created_at GROUP BY crate_date ORDER BY crate_date ASC;
--- Code/scripts/ruf_analysis.sql
+++ Code/scripts/ruf_analysis.sql
@@ -1,26 +1,59 @@
--- RUF propagation (Conditional)
-SELECT  status, COUNT (DISTINCT(version_from)) FROM dep_version_feature 
-INNER JOIN feature_status ON name=nightly_feature GROUP BY status;
+-- 1. RUF lifetime (status)
 
--- RUF propagation (Unconditional)
+SELECT COUNT(*) FROM feature_timeline;
+SELECT status, COUNT(*) FROM feature_status GROUP BY status;
+
+
+-- 2. RUF Usage
+-- Used RUF
+SELECT COUNT(*) FROM feature_status
+  WHERE name in (SELECT DISTINCT feature FROM version_feature_ori);
+SELECT status, COUNT(*) FROM feature_status
+  WHERE name in (SELECT DISTINCT feature FROM version_feature_ori)
+  GROUP BY status;
+-- Versions that use RUF
+SELECT COUNT(DISTINCT id) FROM version_feature_ori INNER JOIN feature_status ON name=feature; 
+SELECT status, COUNT(DISTINCT id) FROM version_feature_ori INNER JOIN feature_status ON name=feature GROUP BY status;
+SELECT COUNT(DISTINCT id) FROM version_feature_ori INNER JOIN feature_status ON name=feature WHERE status != 'accepted';  
+SELECT COUNT(DISTINCT id) FROM version_feature_ori INNER JOIN feature_status ON name=feature WHERE status = 'removed' OR status IS NULL;  
+-- RUF Usage Items
+SELECT COUNT(*) FROM version_feature_ori INNER JOIN feature_status ON name=feature; 
+SELECT status, COUNT(*) FROM version_feature_ori INNER JOIN feature_status ON name=feature GROUP BY status; 
+
+
+-- 3. RUF Impacts
+-- Direct Impact
+SELECT COUNT(*) FROM feature_status
+  WHERE name in (SELECT DISTINCT feature FROM version_feature_ori);
+SELECT status, COUNT(*) FROM feature_status
+  WHERE name in (SELECT DISTINCT feature FROM version_feature_ori)
+  GROUP BY status;
+-- Unconditional Impact
+WITH uncon_ver AS
+  (SELECT id, status FROM version_feature_ori INNER JOIN feature_status
+  ON name=feature WHERE conds = '' AND feature is not NULL)
+SELECT COUNT(DISTINCT version_from) FROM uncon_ver INNER JOIN dep_version ON
+version_to=id;
 WITH uncon_ver AS
   (SELECT id, status FROM version_feature_ori INNER JOIN feature_status
   ON name=feature WHERE conds = '' AND feature is not NULL)
 SELECT status, COUNT(DISTINCT version_from) FROM uncon_ver INNER JOIN dep_version ON
 version_to=id GROUP BY status;
-
--- RUF propagation (Simple Conditional)
+-- Simple Conditional Impact: Includes Uncond and other easily identified impacts.
 WITH uncon_ver AS
   (SELECT id, status FROM version_feature INNER JOIN feature_status 
-  ON name=feature WHERE conds = '' AND feature != 'no_feature_used')
+  ON name=feature WHERE conds = '' AND feature is not NULL)
+SELECT COUNT(DISTINCT version_from) FROM uncon_ver INNER JOIN dep_version ON
+version_to=id;
+WITH uncon_ver AS
+  (SELECT id, status FROM version_feature INNER JOIN feature_status 
+  ON name=feature WHERE conds = '' AND feature is not NULL)
 SELECT status, COUNT(DISTINCT version_from) FROM uncon_ver INNER JOIN dep_version ON
 version_to=id GROUP BY status;
-
--- RUF usage
-SELECT  status, COUNT( DISTINCT id) FROM version_feature INNER JOIN feature_status 
-ON name=feature WHERE feature != 'no_feature_used' GROUP BY status;
-
--- Total
+-- Indirect Impact (Associate with package feature)
+SELECT  status, COUNT (DISTINCT(version_from)) FROM dep_version_feature 
+INNER JOIN feature_status ON name=nightly_feature GROUP BY status;
+-- Conditional Impact : SimpleCond+IndirImpact
 DROP TABLE IF EXISTS tmp_ruf_impact;
 CREATE TABLE tmp_ruf_impact AS (
     SELECT DISTINCT status, version_from as id FROM dep_version_feature 
@@ -28,40 +61,122 @@ CREATE TABLE tmp_ruf_impact AS (
 );
 WITH uncon_ver AS
   (SELECT id, status FROM version_feature INNER JOIN feature_status 
-  ON name=feature WHERE conds = '' AND feature != 'no_feature_used')
+  ON name=feature WHERE conds = '' AND feature IS NOT NULL)
 INSERT INTO tmp_ruf_impact
-SELECT DISTINCT status,  version_from FROM uncon_ver INNER JOIN dep_version ON version_to=id;
-INSERT INTO tmp_ruf_impact
-SELECT  DISTINCT status,  id FROM version_feature INNER JOIN feature_status 
-ON name=feature WHERE feature != 'no_feature_used';
+  SELECT DISTINCT status,  version_from FROM uncon_ver INNER JOIN dep_version ON version_to=id;
 SELECT status, COUNT(DISTINCT id) FROM tmp_ruf_impact GROUP BY status;
-
-
-
--- Hot RUF (Indir dep)
-DROP TABLE IF EXISTS tmp_hot_ruf_indir;
-CREATE TABLE tmp_hot_ruf_indir AS (
-    SELECT DISTINCT nightly_feature, version_from as id FROM dep_version_feature 
-    INNER JOIN feature_status ON name=nightly_feature
+SELECT COUNT(DISTINCT id) FROM tmp_ruf_impact;
+-- Total Impact: Direct+Cond
+DROP TABLE IF EXISTS tmp_ruf_impact;
+CREATE TABLE tmp_ruf_impact AS (
+    SELECT DISTINCT status, version_from as id FROM dep_version_feature 
+    INNER JOIN feature_status ON name=nightly_feature 
 );
 WITH uncon_ver AS
-  (SELECT DISTINCT id, feature FROM version_feature INNER JOIN feature_status 
-  ON name=feature WHERE conds = '' AND feature != 'no_feature_used')
-INSERT INTO tmp_hot_ruf_indir
-SELECT DISTINCT feature, version_from FROM uncon_ver INNER JOIN dep_version ON version_to=id;
-WITH hot_ruf AS(SELECT nightly_feature, COUNT(DISTINCT id) FROM tmp_hot_ruf_indir GROUP BY nightly_feature)
-SELECT nightly_feature, count, status FROM hot_ruf INNER JOIN feature_status
-ON  nightly_feature = name ORDER BY count DESC;
+  (SELECT id, status FROM version_feature INNER JOIN feature_status 
+  ON name=feature WHERE conds = '' AND feature IS NOT NULL)
+INSERT INTO tmp_ruf_impact
+  SELECT DISTINCT status,  version_from FROM uncon_ver INNER JOIN dep_version ON version_to=id;
+INSERT INTO tmp_ruf_impact
+  SELECT  DISTINCT status,  id FROM version_feature INNER JOIN feature_status 
+  ON name=feature WHERE feature IS NOT NULL;
+SELECT status, COUNT(DISTINCT id) FROM tmp_ruf_impact GROUP BY status;
+SELECT COUNT(DISTINCT id) FROM tmp_ruf_impact;
+SELECT COUNT(DISTINCT id) FROM tmp_ruf_impact WHERE status = 'removed' OR status IS NULL; -- Extra: In Total Impact, sum of Unknown and Removed RUF Impact.
+
+
+-- 4. Hot version using RUF (using certain status of RUF)
+-- Top dependents
+WITH deps AS (
+    SELECT version_to, COUNT(DISTINCT version_from) as dependents
+    FROM dep_version
+    WHERE version_to IN(
+        SELECT id FROM version_feature INNER JOIN feature_status 
+        ON name=feature WHERE conds = '' AND feature != 'no_feature_used' AND status = 'xxx'
+    ) GROUP BY version_to
+)
+SELECT deps.*, crate_id, num ,name
+FROM deps INNER JOIN versions_with_name ON id=version_to ORDER BY dependents DESC;
+-- Hot RUF by usage
+WITH hot_feat AS(
+    SELECT feature, COUNT(*) FROM version_feature WHERE feature IS NOT NULL GROUP BY feature 
+) SELECT feature, count, status FROM hot_feat INNER JOIN feature_status ON feature = name ORDER BY count DESC;
+-- Abnormal Hot RUF by usage
+WITH hot_feat AS(
+    SELECT feature, COUNT(*) FROM version_feature WHERE feature IS NOT NULL GROUP BY feature 
+) SELECT feature, count, status FROM hot_feat INNER JOIN feature_status ON feature = name 
+WHERE feature IN (SELECT * FROM feature_abnormal)
+ORDER BY count DESC;
+-- Hot RUF by dependents
+WITH hot_feat AS(
+    SELECT feature, COUNT(DISTINCT version_from) FROM version_feature INNER JOIN dep_version ON id=version_to
+    WHERE conds = '' GROUP BY feature 
+) SELECT feature, count, status FROM hot_feat INNER JOIN feature_status ON feature = name ORDER BY count DESC;
+-- Abnormal Hot RUF by dependents
+WITH hot_feat AS(
+    SELECT feature, COUNT(DISTINCT version_from) FROM version_feature INNER JOIN dep_version ON id=version_to
+    WHERE conds = '' GROUP BY feature 
+) SELECT feature, count, status FROM hot_feat INNER JOIN feature_status ON feature = name
+WHERE feature IN (SELECT * FROM feature_abnormal)
+ORDER BY count DESC;
 
 
 
-SELECT COUNT(DISTINCT crate_id) 
-FROM dep_version_feature INNER JOIN versions ON version_from = id
-WHERE crate_id NOT IN (
-    WITH uncon_ver AS (
-    SELECT DISTINCT version_to FROM dep_version WHERE version_to IN(
-        SELECT id FROM version_feature WHERE conds = '' AND feature != 'no_feature_used'
-    ))
-    SELECT DISTINCT crate_id FROM uncon_ver INNER JOIN versions
-        ON version_to=id
+-- 5. Hot version(RUF) with most dependents
+WITH uncon_ver_sta AS (
+	WITH uncon_ver AS (
+		SELECT versions_with_name.id, name, num, feature FROM version_feature_ori INNER JOIN versions_with_name
+			ON version_feature_ori.id = versions_with_name.id WHERE conds = '' AND feature is not NULL)
+	SELECT id, uncon_ver.name, num, feature, status FROM uncon_ver INNER JOIN feature_status 
+		ON uncon_ver.feature = feature_status.name)
+, indir_dep AS (
+	SELECT version_to, COUNT(DISTINCT version_from) FROM dep_version WHERE version_to IN(
+    SELECT id FROM version_feature_ori WHERE conds = '' AND feature is not NULL
+	) GROUP BY version_to)
+SELECT uncon_ver_sta.name, num, count AS dependents, feature, status
+FROM uncon_ver_sta INNER JOIN indir_dep ON id=version_to
+ORDER BY dependents DESC
+
+
+
+-- 6. RUF Evolution: Only considers direct usage rather than indirect impacts.ADD
+-- The results show the proportion of versions using RUF is declining, while we found that impacts are rising under limited study.
+
+SELECT versions.id, feature, created_at FROM version_feature INNER JOIN versions
+ON version_feature.id = versions.id WHERE feature != 'no_feature_used' AND created_at < '2022-06-01' ORDER BY created_at DESC;
+
+DROP TABLE IF EXISTS tmp_year_month;
+CREATE TABLE tmp_year_month(
+    crate_date date
 );
+INSERT INTO tmp_year_month VALUES
+    ('2015-01-01'), ('2015-04-01'), ('2015-07-01'), ('2015-10-01'),
+    ('2016-01-01'), ('2016-04-01'), ('2016-07-01'), ('2016-10-01'),
+    ('2017-01-01'), ('2017-04-01'), ('2017-07-01'), ('2017-10-01'),
+    ('2018-01-01'), ('2018-04-01'), ('2018-07-01'), ('2018-10-01'),
+    ('2019-01-01'), ('2019-04-01'), ('2019-07-01'), ('2019-10-01'),
+    ('2020-01-01'), ('2020-04-01'), ('2020-07-01'), ('2020-10-01'),
+    ('2021-01-01'), ('2021-04-01'), ('2021-07-01'), ('2021-10-01'),
+    ('2022-01-01'), ('2022-04-01'), ('2022-07-01'), ('2022-08-12')
+;
+
+-- Version (ruf)
+WITH all_ruf AS(
+  SELECT versions.id, feature, created_at FROM version_feature INNER JOIN versions
+ON version_feature.id = versions.id WHERE feature != 'no_feature_used'
+) 
+SELECT crate_date, COUNT(DISTINCT id) FROM all_ruf INNER JOIN tmp_year_month
+ON crate_date > created_at GROUP BY crate_date ORDER BY crate_date ASC;
+
+-- Crate (ruf)
+WITH all_ruf AS(
+  SELECT versions_with_name.crate_id, feature, created_at FROM version_feature INNER JOIN versions_with_name
+ON version_feature.id = versions_with_name.id WHERE feature != 'no_feature_used'
+) 
+SELECT crate_date, COUNT(DISTINCT crate_id) FROM all_ruf INNER JOIN tmp_year_month
+ON crate_date > created_at GROUP BY crate_date ORDER BY crate_date ASC;
+
+-- Version (all)
+
+SELECT crate_date, COUNT(DISTINCT id) FROM versions INNER JOIN tmp_year_month
+ON crate_date > created_at GROUP BY crate_date ORDER BY crate_date ASC;
