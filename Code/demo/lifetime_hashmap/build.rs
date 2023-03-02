64a864392ae5cb0ca61821b97e0aa38c24dd6eee
LoanCold <loancold@foxmail.com>
2023-03-02 13:35:09 +0800

    auto build ruf lifetime table

---
+++ Code/demo/lifetime_hashmap/build.rs
@@ -0,0 +1,9 @@
+use std::process::Command;
+use std::env;
+use std::path::Path;
+
+fn main() {
+    // Command::new("python3").args(&["build-lifetime-hashmap.py"])
+    //                    .status().unwrap();
+    // println!("cargo:rerun-if-changed=build-lifetime-hashmap.py");
+}
