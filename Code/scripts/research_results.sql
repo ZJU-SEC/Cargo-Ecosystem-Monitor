f039f4c8c15ec4b40acdceaedfca8bf46e4b3090
loancold <loancold@foxmail.com>
2023-06-13 10:31:31 +0800

    Better result display format

--- Code/scripts/research_results.sql
+++ Code/scripts/research_results.sql
@@ -1,7 +1,7 @@
 -- Result 1.1: RUF Status
-SELECT count(*), v1_63_0 FROM feature_timeline GROUP BY v1_63_0;
+SELECT count(*), v1_63_0 as status FROM feature_timeline GROUP BY v1_63_0;
 -- Result 1.1 (Complete): RUF Status
-SELECT name, v1_63_0 FROM feature_timeline ;
+SELECT name, v1_63_0 as status FROM feature_timeline ;
 
 -- Result 1.2 (Complete): Abnormal RUF Lifetime.
 SELECT feature_timeline.* FROM feature_abnormal INNER JOIN feature_timeline ON feature_timeline.name = feature_abnormal.name;
@@ -12,7 +12,7 @@ SELECT status, COUNT(*) FROM feature_status
   WHERE name in (SELECT DISTINCT feature FROM version_feature_ori)
   GROUP BY status;
 -- RUF count (Complete)
-SELECT status, * FROM feature_status
+SELECT name, status FROM feature_status
   WHERE name in (SELECT DISTINCT feature FROM version_feature_ori);
 -- Package Versions
 SELECT status, COUNT(DISTINCT id) FROM version_feature_ori INNER JOIN feature_status ON name=feature GROUP BY status;
@@ -31,41 +31,45 @@ SELECT status, COUNT(DISTINCT version_from) FROM uncon_ver INNER JOIN dep_versio
 version_to=id GROUP BY status;
 -- Uncond Impact (Complete)
 WITH uncon_ver AS
-  (SELECT id, status FROM version_feature_ori INNER JOIN feature_status
+  (SELECT id, name as ruf, status FROM version_feature_ori INNER JOIN feature_status
   ON name=feature WHERE conds = '' AND feature is not NULL)
-SELECT DISTINCT version_from, status FROM uncon_ver INNER JOIN dep_version ON
+SELECT DISTINCT version_from, ruf, status FROM uncon_ver INNER JOIN dep_version ON
 version_to=id;
+
 -- Cond Impact
 DROP TABLE IF EXISTS tmp_ruf_impact;
 CREATE TABLE tmp_ruf_impact AS (
-    SELECT DISTINCT status, version_from as id FROM dep_version_feature 
+    SELECT DISTINCT version_from as ver, nightly_feature as ruf, status FROM dep_version_feature 
     INNER JOIN feature_status ON name=nightly_feature 
 );
 WITH uncon_ver AS
-  (SELECT id, status FROM version_feature INNER JOIN feature_status 
-  ON name=feature WHERE conds = '' AND feature IS NOT NULL)
+  (SELECT id, name as ruf, status FROM version_feature INNER JOIN feature_status
+  ON name=feature WHERE conds = '' AND feature is not NULL)
 INSERT INTO tmp_ruf_impact
-  SELECT DISTINCT status,  version_from FROM uncon_ver INNER JOIN dep_version ON version_to=id;
-SELECT status, COUNT(DISTINCT id) FROM tmp_ruf_impact GROUP BY status;
+  SELECT DISTINCT version_from, ruf, status FROM uncon_ver INNER JOIN dep_version ON
+  version_to=id;
+SELECT status, COUNT(DISTINCT ver) FROM tmp_ruf_impact GROUP BY status;
 -- Cond Impact (Complete)
-SELECT DISTINCT id, status FROM tmp_ruf_impact;
+SELECT DISTINCT * FROM tmp_ruf_impact;
+
 -- Total Impact
 DROP TABLE IF EXISTS tmp_ruf_impact;
 CREATE TABLE tmp_ruf_impact AS (
-    SELECT DISTINCT status, version_from as id FROM dep_version_feature 
+    SELECT version_from as ver, nightly_feature as ruf, status FROM dep_version_feature 
     INNER JOIN feature_status ON name=nightly_feature 
 );
 WITH uncon_ver AS
-  (SELECT id, status FROM version_feature INNER JOIN feature_status 
-  ON name=feature WHERE conds = '' AND feature IS NOT NULL)
+  (SELECT id, name as ruf, status FROM version_feature INNER JOIN feature_status
+  ON name=feature WHERE conds = '' AND feature is not NULL)
 INSERT INTO tmp_ruf_impact
-  SELECT DISTINCT status,  version_from FROM uncon_ver INNER JOIN dep_version ON version_to=id;
+  SELECT DISTINCT version_from, ruf, status FROM uncon_ver INNER JOIN dep_version ON
+  version_to=id;
 INSERT INTO tmp_ruf_impact
-  SELECT  DISTINCT status,  id FROM version_feature INNER JOIN feature_status 
+  SELECT  DISTINCT id, feature, status FROM version_feature INNER JOIN feature_status 
   ON name=feature WHERE feature IS NOT NULL;
-SELECT status, COUNT(DISTINCT id) FROM tmp_ruf_impact GROUP BY status;
+SELECT status, COUNT(DISTINCT ver) FROM tmp_ruf_impact GROUP BY status;
 -- Cond Impact (Complete)
-SELECT DISTINCT id, status FROM tmp_ruf_impact;
+SELECT DISTINCT * FROM tmp_ruf_impact;
 
 -- Result 3.2 Super Spreaders
 WITH deps AS (
@@ -73,8 +77,8 @@ WITH deps AS (
     FROM dep_version
     WHERE version_to IN(
         SELECT id FROM version_feature INNER JOIN feature_status 
-        ON name=feature WHERE conds = '' AND feature != 'no_feature_used' AND status = 'removed'
+        ON name=feature WHERE conds = '' AND feature != 'no_feature_used' AND status = 'unknown'
     ) GROUP BY version_to
 )
-SELECT deps.*, crate_id, num ,name
+SELECT name, num as version_num, dependents
 FROM deps INNER JOIN versions_with_name ON id=version_to ORDER BY dependents DESC;
